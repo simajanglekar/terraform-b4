7c0839065bd51fedeea9bd362dc15e9d17b8ea99
simajanglekar <simajanglekar@gmail.com>
2023-04-25 10:08:33 -0700

    new_instance

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Terraform ###
-sima notes
+
+## data type if terraform ##
+1. string ---> # string means combination of charecters Ex. "sima" , "sima123" , "redhat234"
+2. Number = 123, 5674, 9844000
+3. Bool = true, false 0, 1
+4. list = Ex. ["banana", "apple", "papaya"]
+5. Map/Dictionary = Ex. { city = "pune" , state="MH" , counrty= "india" }
---
+++ firts.terraform.script.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+resource "aws_instance" "my-instance" {
+    ami = "ami-0763cf792771fe1bd"
+    instance_type = "t2.micro"
+    key_name = "sima"
+    tags = {
+        env = "dev"
+        Name = "instance-1"
+        vpc_security_group_ids = ["sg-0ef6349993a032265" , "sg-09de3c34c106ef45d"]
+    }
+}
