bb1d09e525d383077902a0b40045f01aaeb5dc82
simajanglekar <simajanglekar@gmail.com>
2023-04-26 05:16:29 -0700

    --- Auto Git Commit ---

--- firts.terraform.script.tf
+++ firts.terraform.script.tf
@@ -6,8 +6,7 @@ resource "aws_instance" "my-instance" {
     instance_type = var.instance_type
     key_name = var.key_pair
     tags = var.tags
-        env = "dev"
-        Name = "instance-1"
+    
     }
         vpc_security_group_ids = ["sg-0ef6349993a032265" , "sg-09de3c34c106ef45d"]
     }
