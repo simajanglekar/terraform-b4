1254c3566d527299f12b91de8065d423431ae272
simajanglekar <simajanglekar@gmail.com>
2023-04-26 06:33:12 -0700

    --- Auto Git Commit ---

--- firts.terraform.script.tf
+++ firts.terraform.script.tf
@@ -10,7 +10,7 @@ provider "aws" {
     region = var.region
 }
 resource "aws_instance" "my-instance" {
-    ami = var.ami.id
+    ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_pair
     tags = var.tags
